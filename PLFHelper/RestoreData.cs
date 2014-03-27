0d0698b48b9106221ab24e2cb7544b43559bf0a6
Oliver Schramm <oliver.schramm97@gmail.com>
2014-03-27 18:50:56 +0100

    Add new class RestoreData
    This is an awesome class.
    It should save all important data which is currently in memory. So it
    would be redundant IF you couldn't serialize it. But you can so you can
    write the whole object to a file and have your memory backup! ;-)
    If the program crashes you can "restore the data" and that's it. :D

---
+++ PLFHelper/RestoreData.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Copyright (c) 2014 Oliver Schramm
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+using Google.GData.Spreadsheets;
+using System;
+
+namespace PLFHelper
+{
+	[Serializable()]
+	internal class RestoreData
+	{
+		public string Game { get; set; }
+
+		public Settings Settings { get; set; }
+
+		public SpreadsheetEntry[] SheetEntries { get; set; }
+
+		public SpreadsheetsService SheetService { get; set; }
+
+		public float[] Values { get; set; }
+
+		public RestoreData(Settings settings, float[] values, string game, SpreadsheetEntry[] sheetEntries, SpreadsheetsService sheetService)
+		{
+			this.Settings = settings;
+			this.Values = values;
+			this.Game = game;
+			this.SheetEntries = sheetEntries;
+			this.SheetService = SheetService;
+		}
+	}
+}
