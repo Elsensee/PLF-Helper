7e4e6c46333e20ec9370a71bb4d164e7e4b8d7b0
Oliver Schramm <oliver.schramm97@gmail.com>
2014-03-30 12:07:47 +0200

    Fixes #7 with the possibility to also look into files that don't have a suffix

--- Parser/LocalizationManager.cs
+++ Parser/LocalizationManager.cs
@@ -102,7 +102,7 @@ namespace Parser
 		/// <exception cref="System.ArgumentNullException">name is null.</exception>
 		public static string GetLocalizedString(string name)
 		{
-			return GetLocalizedString(name, currentCulture);
+			return GetLocalizedString(name, currentCulture, false);
 		}
 
 		/// <summary>
@@ -113,6 +113,19 @@ namespace Parser
 		/// <returns>Returns the localized string in the given culture or in english if it wasn't found in the given culture.</returns>
 		/// <exception cref="System.ArgumentNullException">name is null.</exception>
 		public static string GetLocalizedString(string name, string locale)
+		{
+			return GetLocalizedString(name, locale, false);
+		}
+
+		/// <summary>
+		/// Gets a localized string.
+		/// </summary>
+		/// <param name="name">The name of the string.</param>
+		/// <param name="locale">The two-letter-string of the culture of which the string should be returned.</param>
+		/// <param name="fileWithoutSuffix"><c>false</c> if file should be in the normal format: <c>file_locale.txt</c>, <c>true</c> if it should be just <c>file.txt</c></param>
+		/// <returns>Returns the localized string in the given culture or in english if it wasn't found in the given culture.</returns>
+		/// <exception cref="System.ArgumentNullException">name is null.</exception>
+		private static string GetLocalizedString(string name, string locale, bool fileWithoutSuffix)
 		{
 			// If name is null we won't find any string...
 			if (name == null)
@@ -125,9 +138,11 @@ namespace Parser
 				locale = "en";
 			}
 
+			string file = (fileWithoutSuffix) ? FileName + ".txt" : FileName + "_" + locale + ".txt";
+
 			try
 			{
-				using (var sr = new StreamReader(assembly.GetManifestResourceStream(typeof(LocalizationManager).Namespace + "." + fileName + "_" + locale + ".txt")))
+				using (var sr = new StreamReader(assembly.GetManifestResourceStream(typeof(LocalizationManager).Namespace + "." + file)))
 				{
 					while (!sr.EndOfStream)
 					{
@@ -149,12 +164,16 @@ namespace Parser
 			{
 				if (locale != "en")
 				{
-					return GetLocalizedString(name, "en");
+					return GetLocalizedString(name, "en", false);
+				}
+				else if (locale == "en" && !fileWithoutSuffix)
+				{
+					return GetLocalizedString(name, "en", true);
 				}
 				throw;
 			}
 
-			return (locale != "en") ? GetLocalizedString(name, "en") : null;
+			return (locale != "en") ? GetLocalizedString(name, "en", false) : ((locale == "en" && !fileWithoutSuffix) ? GetLocalizedString(name, "en", true) : null);
 		}
 	}
 }
