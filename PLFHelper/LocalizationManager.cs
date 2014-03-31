3480871b7a471b89153a14578bcaec1c13e36802
Oliver Schramm <oliver.schramm97@gmail.com>
2014-03-31 16:13:05 +0200

    Optimize LocalizationManager again (oops)

--- PLFHelper/LocalizationManager.cs
+++ PLFHelper/LocalizationManager.cs
@@ -102,7 +102,7 @@ namespace PLFHelper
 		/// <exception cref="System.ArgumentNullException">name is null.</exception>
 		public static string GetLocalizedString(string name)
 		{
-			return GetLocalizedString(name, currentCulture, false);
+			return GetLocalizedString(name, currentCulture);
 		}
 
 		/// <summary>
@@ -110,22 +110,12 @@ namespace PLFHelper
 		/// </summary>
 		/// <param name="name">The name of the string.</param>
 		/// <param name="locale">The two-letter-string of the culture of which the string should be returned.</param>
+		/// <param name="fileWithoutSuffix"><c>false</c> if file should be in the normal format: <c>file_locale.txt</c>, <c>true</c> if it should be just <c>file.txt</c>.
+		/// Please don't speify that parameter by yourself.</param>
 		/// <returns>Returns the localized string in the given culture or in english if it wasn't found in the given culture.</returns>
 		/// <exception cref="System.ArgumentNullException">name is null.</exception>
-		public static string GetLocalizedString(string name, string locale)
-		{
-			return GetLocalizedString(name, locale, false);
-		}
-
-		/// <summary>
-		/// Gets a localized string.
-		/// </summary>
-		/// <param name="name">The name of the string.</param>
-		/// <param name="locale">The two-letter-string of the culture of which the string should be returned.</param>
-		/// <param name="fileWithoutSuffix"><c>false</c> if file should be in the normal format: <c>file_locale.txt</c>, <c>true</c> if it should be just <c>file.txt</c></param>
-		/// <returns>Returns the localized string in the given culture or in english if it wasn't found in the given culture.</returns>
-		/// <exception cref="System.ArgumentNullException">name is null.</exception>
-		private static string GetLocalizedString(string name, string locale, bool fileWithoutSuffix)
+		/// <exception cref="System.IO.FileNotFoundException">The file wasn't found in the res</exception>
+		public static string GetLocalizedString(string name, string locale, bool fileWithoutSuffix = false)
 		{
 			// If name is null we won't find any string...
 			if (name == null)
