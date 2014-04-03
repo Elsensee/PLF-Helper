8e32f04404cbe2e5be722c2d6fae9666102883cb
Oliver Schramm <oliver.schramm97@gmail.com>
2014-04-03 20:11:33 +0200

    Fix namespace
    I am very stupid... How could I miss that???

--- Parser/LocalizationManager.cs
+++ Parser/LocalizationManager.cs
@@ -24,7 +24,7 @@ using System.IO;
 using System.Reflection;
 using System.Text.RegularExpressions;
 
-namespace PLFHelper
+namespace Parser
 {
 	/// <summary>
 	/// Own localization manager for getting localized strings.
