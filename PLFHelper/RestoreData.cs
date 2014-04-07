57ff29200b95cd664a1832ee84148b1604a3e9c7
Oliver Schramm <oliver.schramm97@gmail.com>
2014-04-07 23:31:49 +0200

    Add Save methods to RestoreData class
    Haha.. mostly copy & paste xD

--- PLFHelper/RestoreData.cs
+++ PLFHelper/RestoreData.cs
@@ -18,6 +18,9 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security;
 using Google.GData.Spreadsheets;
 using System;
 
@@ -62,6 +65,77 @@ namespace PLFHelper
 			this.Values = values;
 			this.SheetEntries = sheetEntries;
 			this.SheetService = SheetService;
+		}
+
+		/// <summary>
+		/// Saves the current settings object to <paramref name="path"/>.
+		/// </summary>
+		/// <param name="path">The path, the current settings object should be saved to.</param>
+		/// <exception cref="System.ArgumentNullException"><paramref name="path"/> is null.</exception>
+		public void Save(string path)
+		{
+			SaveRestoreData(this, path);
+		}
+
+		/// <summary>
+		/// Saves the given <paramref name="restoreDataObject"/> to <paramref name="path"/>.
+		/// </summary>
+		/// <param name="restoreDataObject">The RestoreData object that sould be saved.</param>
+		/// <param name="path">The path, the given RestoreData object should be saved to.</param>
+		/// <exception cref="System.ArgumentNullException"><paramref name="restoreDataObject"/> or <paramref name="path"/> is null.</exception>
+		public static void SaveRestoreData(RestoreData restoreDataObject, string path)
+		{
+			if (restoreDataObject == null)
+			{
+				throw new ArgumentNullException("restoreDataObject");
+			}
+			if (path == null)
+			{
+				throw new ArgumentNullException("path");
+			}
+
+			try
+			{
+				using (var fileStream = new FileStream(path, FileMode.Create))
+				{
+					// Yup - we don't need that object after that so just... don't save it
+					new BinaryFormatter().Serialize(fileStream, restoreDataObject);
+				}
+			}
+			// Not the finest way, but it's okay to just say nothing...
+			// We don't want to bother the user with that.
+			catch (SecurityException) { }
+		}
+
+		/// <summary>
+		/// Loads a RestoreData object from a given <paramref name="path"/>.
+		/// </summary>
+		/// <param name="path">The path, the RestoreData object should be loaded from.</param>
+		/// <returns>A new RestoreData object with the data loaded from the file given in <paramref name="path"/>.</returns>
+		/// <exception cref="System.ArgumentNullException"><paramref name="path"/> is null.</exception>
+		/// <exception cref="System.IO.FileNotFoundException">The file in <paramref name="path"/> does not exist.</exception>
+		public static RestoreData LoadRestoreData(string path)
+		{
+			if (path == null)
+			{
+				throw new ArgumentNullException("path");
+			}
+
+			try
+			{
+				using (var fileStream = new FileStream(path, FileMode.Open))
+				{
+					// FileStream will be closed anyway.
+					return (RestoreData) new BinaryFormatter().Deserialize(fileStream);
+				}
+			}
+			// See above...
+			catch (SecurityException)
+			{
+				// we have to return something... but we can't do some random settings...
+				// so we just return null.
+				return null;
+			}
 		}
 	}
 }
